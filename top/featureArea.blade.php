87fce3c12d286b905f4118970b703dc01b7c14b9
Y-Takuho <79884503+Y-Takuho@users.noreply.github.com>
2026-04-01 16:15:24 +0900

    Update top featureArea.blade.php

--- top/featureArea.blade.php
+++ top/featureArea.blade.php
@@ -6,41 +6,42 @@
 
     <div>
       <ul class="banner-panel-list">
-        <!-- 1. 【リラク・エステ】得とく -->
+                <!-- 1. 【リラク・エステ】得とく -->
         <li class="banner-panel-list__two-item">
-          <a class="banner-panel-list__two-item--link" href="https://mitsuraku.jp/lpe/tktk50/?media=faspa&utm_source=epark.jp&utm_medium=banner&utm_campaign=tktk50&utm_term=202503&utm_content=toprecommend" target="_blank">
-            <img loading="lazy" src="{{ asset('/v2/sfc/images/feature/rae_tokutoku_720x432_250218.png') . '?ver=' . config('const.cache_clear_ver') }}" alt="【リラク・エステ】得とく" class="banner-panel-list__two-item--image">
+          <a class="banner-panel-list__two-item--link" href="https://mitsuraku.jp/lpe/tktk50/?media=faspa&amp;utm_source=epark.jp&amp;utm_medium=banner&amp;utm_campaign=tktk50&amp;utm_term=202503&amp;utm_content=toprecommend" target="_blank">
+            <img loading="lazy" src="{{ asset(&#39;/v2/sfc/images/feature/rae_tokutoku_720x432_250218.png&#39;) . &#39;?ver=&#39; . config(&#39;const.cache_clear_ver&#39;) }}" alt="【リラク・エステ】得とく" class="banner-panel-list__two-item--image">
             <p class="banner-panel-list__two-item--text">【リラク・エステ】月10名さま限定★50％OFF！対象サロンの特別メニューがお得にご利用いただけます♪</p>
           </a>
         </li>
 
-        <!-- 2. 【EPARK】 Premium Club  -->
+        <!-- 2. 【ヘアサロン】Ozmall 新春運試し -->
         <li class="banner-panel-list__two-item">
-          <a class="banner-panel-list__two-item--link" href="https://premiumclub.epark.jp/?utm_source=epark.jp&utm_medium=banner&utm_campaign=premiumclub&utm_term=2025&utm_content=toprecommend" target="_blank">
-            <img loading="lazy" src="{{ asset('/v2/sfc/images/feature/epc_251229_720x432.png') . '?ver=' . config('const.cache_clear_ver') }}" alt="【ヘアサロン】Ozmall 新春運試し " class="banner-panel-list__two-item--image">
+          <a class="banner-panel-list__two-item--link" href="https://premiumclub.epark.jp/?utm_source=epark.jp&amp;utm_medium=banner&amp;utm_campaign=premiumclub&amp;utm_term=2025&amp;utm_content=toprecommend" target="_blank">
+            <img loading="lazy" src="{{ asset(&#39;/v2/sfc/images/feature/epc_251229_720x432.png&#39;) . &#39;?ver=&#39; . config(&#39;const.cache_clear_ver&#39;) }}" alt="【ヘアサロン】Ozmall 新春運試し" class="banner-panel-list__two-item--image">
             <p class="banner-panel-list__two-item--text">【EPARK Premium Club】携帯補償つきでパワーアップ！初月無料で今すぐ体験！</p>
           </a>
         </li>
 
-        <!-- 3.【EPARK】 Premium Club -->
+        <!-- 3. ヘアサロン(OZmall) 新春運試し -->
         <li class="banner-panel-list__three-item">
-          <a class="banner-panel-list__three-item--link" href="https://www.ozmall.co.jp/countprogram/entry/?affiliateId=556&entryPageId=0&entryPageUrl=/hairsalon/feature/44820/&scid=salp01057beha" target="_blank">
-            <img loading="lazy" src="{{ asset('/v2/sfc/images/feature/New-life-support__2603_720×432.jpg') . '?ver=' . config('const.cache_clear_ver') }}" alt="ヘアサロン(OZmall) 新春運試し" class="banner-panel-list__three-item--image">
+          <a class="banner-panel-list__three-item--link" href="https://www.ozmall.co.jp/countprogram/entry/?affiliateId=556&amp;entryPageId=0&amp;entryPageUrl=/hairsalon/feature/44820/&amp;scid=salp01057beha" target="_blank">
+            <img loading="lazy" src="{{ asset(&#39;/v2/sfc/images/feature/New-life-support__2603_720×432.jpg&#39;) . &#39;?ver=&#39; . config(&#39;const.cache_clear_ver&#39;) }}" alt="ヘアサロン(OZmall) 新春運試し" class="banner-panel-list__three-item--image">
             <p class="banner-panel-list__three-item--text">春は出会いの季節。ヘアサロンで整えて第一印象をアップ。EPARK会員限定1,000円オフで新生活をキレイに始めよう。</p>
           </a>
         </li>
-        <!-- 4. 【接骨・鍼灸】ギフト券 -->
+
+        <!-- 4. 【接骨・鍼灸】ギフト券抽選キャンペーン -->
         <li class="banner-panel-list__three-item">
-          <a class="banner-panel-list__three-item--link" href="https://karadarefre.jp/cp/hanamatsuri_lottery_campaign_26?site_code=faspa&utm_source=epark.jp&utm_medium=banner&utm_content=toprecommend&utm_campaign=hanamatsuri_lottery_campaign_26" target="_blank">
-            <img loading="lazy" src="{{ asset('/v2/sfc/images/feature/skt_720x432_2603.png') . '?ver=' . config('const.cache_clear_ver') }}" alt="【接骨・鍼灸】ギフト券抽選キャンペーン" class="banner-panel-list__three-item--image">
+          <a class="banner-panel-list__three-item--link" href="https://karadarefre.jp/cp/hanamatsuri_lottery_campaign_26?site_code=faspa&amp;utm_source=epark.jp&amp;utm_medium=banner&amp;utm_content=toprecommend&amp;utm_campaign=hanamatsuri_lottery_campaign_26" target="_blank">
+            <img loading="lazy" src="{{ asset(&#39;/v2/sfc/images/feature/skt_720x432_2603.png&#39;) . &#39;?ver=&#39; . config(&#39;const.cache_clear_ver&#39;) }}" alt="【接骨・鍼灸】ギフト券抽選キャンペーン" class="banner-panel-list__three-item--image">
             <p class="banner-panel-list__three-item--text">【接骨・鍼灸】対象期間内に特設サイト上でのエントリー＋ネット予約＆来店された方へ、抽選で最大1万円分のギフト券をプレゼント♪</p>
           </a>
         </li>
-        
-        <!-- 5. 【EPARK】サービス向上アンケート -->
+
+        <!-- 5. 【薬局】Amazonギフトカード -->
         <li class="banner-panel-list__three-item">
-          <a class="banner-panel-list__three-item--link" href="https://www.kusurinomadoguchi.com/questionnaire/campaign/all-users2-2102?utm_source=epark.jp&utm_medium=eparkcampaign&utm_campaign=all-users2-2102" target="_blank">
-            <img loading="lazy" src="{{ asset('/v2/sfc/images/feature/ksr_240920_720-432.png') . '?ver=' . config('const.cache_clear_ver') }}" alt="【薬局】Amazonギフトカード" class="banner-panel-list__three-item--image">
+          <a class="banner-panel-list__three-item--link" href="https://www.kusurinomadoguchi.com/questionnaire/campaign/all-users2-2102?utm_source=epark.jp&amp;utm_medium=eparkcampaign&amp;utm_campaign=all-users2-2102" target="_blank">
+            <img loading="lazy" src="{{ asset(&#39;/v2/sfc/images/feature/ksr_240920_720-432.png&#39;) . &#39;?ver=&#39; . config(&#39;const.cache_clear_ver&#39;) }}" alt="【薬局】Amazonギフトカード" class="banner-panel-list__three-item--image">
             <p class="banner-panel-list__three-item--text">【もれなく全員対象】薬局を利用しアンケート回答でAmazonギフトカード500円分が当たるキャンペーン開催中！</p>
           </a>
         </li>
